19a24e863a6be2dd9fdb645e3389f280a279592b
Manuel Montoya <montoya.christensen@outlook.com>
2025-10-15 04:22:24 -0300

    Fix

--- app/views/SellerView.php
+++ app/views/SellerView.php
@@ -1,20 +1,21 @@
 <?php
-class SellerView{
-    public function showSellers($sellers){
+class SellerView
+{
+    public function showSellers($sellers)
+    {
         $count = count($sellers);
         require_once './templates/sellers-list.php';
     }
 
-    public function showEditMenu($sellerToEdit, $sellers){
+    public function showEditMenu($sellerToEdit, $sellers)
+    {
         $count = count($sellers);
         require_once './templates/sellers-edit-menu.php';
     }
 
-    public function showFormAddSeller(){
+    public function showFormAddSeller()
+    {
         require_once './templates/form-addSeller.phtml';
     }
 
-    public function showSalesBySeller() {
-        
-    }
 }
--- route.php
+++ route.php
@@ -6,6 +6,8 @@ error_reporting(E_ALL);
 require_once './app/controllers/SaleController.php';
 require_once './app/controllers/SellerController.php';
 require_once './app/controllers/AuthController.php';
+require_once './app/middlewares/GuardMiddleware.php';
+require_once './app/middlewares/SessionMiddleware.php';
 session_start();
 
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
@@ -17,23 +19,32 @@ if (!empty($_GET['action'])) {
 
 $params = explode('/', $action);
 
+// --- Aplico el middleware de sesión ---
+$request = new StdClass();
+$request = (new SessionMiddleware())->run($request);
+
 switch ($params[0]) {
+
     case 'home':
         $controller = new SaleController();
         $controller->showSales();
         break;
+
     case 'vendedores':
         $controller = new SellerController();
         $controller->showSellers();
         break;
-        
-    case 'vendedor': // para ver ventas por vendedor
-        if (!empty($params[1])){
-            $id = (int)$params[1];
-            $controller = new SaleController();
+
+    case 'vendedor':
+        $controller = new SaleController();
+        if (!empty($params[1])) {
+            $id = (int) $params[1];
             $controller->showSalesById($id);
-        }
-    case 'venta': //por id
+        } else
+            echo '404 not found';
+        break;
+
+    case 'venta': 
         $controller = new SaleController();
         if (isset($params[1])) {
             $id = $params[1];
@@ -42,75 +53,94 @@ switch ($params[0]) {
             $controller->showSales();
         }
         break;
+
     case 'addVenta':
+        $request = (new GuardMiddleware())->run($request);
         $controller = new SaleController();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $controller->addSale();
+            $controller->addSale($request);
         } else {
-            $controller->showAddSaleForm();
+            $controller->showAddSaleForm($request);
         }
         break;
-        case 'editarVenta':
-            $controller = new SaleController();
-            if (!empty($params[1])) {
-                $id = $params[1];
-                $controller->showFormUpdate($id);
-            } else {
-                echo "ID no especificado"; //luego borrar
-            }
-            break;
+
+    case 'editarVenta':
+        $request = (new GuardMiddleware())->run($request);
+        $controller = new SaleController();
+        if (!empty($params[1])) {
+            $id = $params[1];
+            $controller->showFormUpdate($id, $request);
+        } else {
+            echo "ID no especificado";
+        }
+        break;
+
     case 'updateSale':
-        if(!empty($params[1])){
+        if (!empty($params[1])) {
             $id = $params[1];
             $controller = new SaleController();
-            $controller->updateSale($id);
+            $controller->updateSale($id, $request);
         }
         break;
+
     case 'deleteSale':
-        if(!empty($params[1])){
+        if (!empty($params[1])) {
             $id = $params[1];
             $controller = new SaleController();
             $controller->deleteSale($id);
         }
         break;
+
     case 'nuevoVendedor':
+        $request = (new GuardMiddleware())->run($request);
         $controller = new SellerController();
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controller->addSeller($request);
+        } else {
+            $controller->showNewSellerForm($request);
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST')
             $controller->addSeller();
-        else 
+        else
             $controller->showNewSellerForm();
-        
+
         break;
+
     case 'editarVendedor':
+        $request = (new GuardMiddleware())->run($request);
         if (!empty($params[1])) {
             $controller = new SellerController();
-            $sellerId = (int)$params[1]; // casteo el id del vendedor
-            $controller->showSellerEditionMenu($sellerId); // le paso el id del vendedor a editar
+            $sellerId = (int) $params[1];
+            $controller->showSellerEditionMenu($sellerId, $request);
         }
         break;
+
     case 'updateSeller':
         if (!empty($params[1])) {
             $controller = new SellerController();
-            $sellerId = (int)$params[1]; // casteo el id del vendedor
-
-            $controller->updateSeller($sellerId);
-        }
-        break;  
-    case 'deleteSeller':
-        if (!empty($params[1])){
-            $id = (int)$params[1];
-            $controller = new SellerController();
-            $controller->deleteSeller($id);
+            $sellerId = (int) $params[1];
+            $controller->updateSeller($sellerId, $request);
         }
         break;
+
+    case 'deleteSeller':
+        if (!empty($params[1])) {
+            $id = (int) $params[1];
+            $controller = new SellerController();
+            $controller->deleteSeller($id, $request);
+        }
+        break;
+
     case 'showLogin':
         $controller = new AuthController();
         $controller->showLogin();
         break;
+
     case 'login':
         $controller = new AuthController();
         $controller->login();
         break;
+
     default:
         echo 'Error!';
         break;
